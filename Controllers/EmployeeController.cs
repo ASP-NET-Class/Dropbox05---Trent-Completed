d8c077a44e4435c8f3a0c6ec91d69c08e4d882f0
Trent Lyons <tlyons1@cougars.ccis.edu>
2020-10-03 21:20:47 -0500

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Dropbox05.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Dropbox05.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        EmployeeDbContext Db;
+        public EmployeeController(EmployeeDbContext db)
+        {
+            this.Db = db;
+        }
+        public IActionResult AllEmployee()
+        {
+            return View(Db.Employees);
+        }
+        // GET: /<controller>/
+        public IActionResult AddEmployee()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddEmployee(Employee employee)
+        {
+            Db.Add(employee);
+            Db.SaveChanges();
+            return RedirectToAction("AllEmployee");
+        }
+        public IActionResult EditEmployee(int id)
+        {
+            Employee employee;
+            employee = Db.Employees.Find(id);
+            return View(employee);
+        }
+        [HttpPost]
+        public IActionResult DeleteEmployee(Employee employee)
+        {
+            Db.Remove(employee);
+            Db.SaveChanges();
+            return RedirectToAction("AllEmployee");
+        }
+    }
+}
---
+++ Models/EmployeeDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dropbox05.Models
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options)
+            : base(options)
+        { }
+    }
+}
